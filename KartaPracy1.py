439420d3a6f6716f1b07ca9a0c9f1c13466e2d01
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-03 11:19:32 +0000

    Zrobiono zad 1-4 z KP1

---
+++ KartaPracy1.py
@@ -0,0 +1,21 @@
+# Zad 1
+
+# a = int(input())
+# b = int(input())
+# print(a ** 2 + b ** 2)
+
+# Zad 2
+
+# a = int   (input())
+# b = int(input())
+# print((a + b) ** 2)
+
+# Zad 3
+
+# a, b = int(input()), int(input())
+# print((a - b) ** 3)
+
+# Zad 4
+
+# a, b, c = map(int,input().split())
+# print(a * b * c)
