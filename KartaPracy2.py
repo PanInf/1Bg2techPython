6b0f7f75286662d038d5a825e44b681140aafe23
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-05 11:19:49 +0000

    Dodano 4 zadania do KP2

---
+++ KartaPracy2.py
@@ -0,0 +1,29 @@
+# Zad 1
+# a = int(input("Podaj liczbe "))
+# if a % 3 == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 2
+# a = int(input("Zapodaj liczbę: "))
+# if a>=100 and a<1000 and a%17==0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 3
+# wiek = int(input("Ile masz lat?\n"))
+# if wiek >= 18:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 4
+LIMIT = 20
+waga = int(input("Ile waży ten gruzowóz?"))
+if waga<=LIMIT:
+    print("A jedz")
+else:
+    print("Bezapelacyjny STOP")
+    
