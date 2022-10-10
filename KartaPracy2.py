d7ab85b6a8e3c53a86ca88773a8e7e150d9d89bb
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-10 11:18:47 +0000

    Dokończono KartePracy2.py (wszystko)

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -20,10 +20,54 @@
 #     print("NIE")
 
 # Zad 4
-LIMIT = 20
-waga = int(input("Ile waży ten gruzowóz?"))
-if waga<=LIMIT:
-    print("A jedz")
-else:
-    print("Bezapelacyjny STOP")
-    
+# LIMIT = 20
+# waga = int(input("Ile waży ten gruzowóz?"))
+# if waga<=LIMIT:
+#     print("A jedz")
+# else:
+#     print("Bezapelacyjny STOP")
+
+# Ankieta
+# User wprowadza dwie liczby - 
+# sprawdź czy pierwsza jest większa od drugiej
+# WE: x, y
+# WY: TAK / NIE
+
+# a, b = int(input()), int(input())
+# if a > b:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# # if (a < c and c < b) or (a < c and c < a):
+# if a < c < b or b < c < a:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+
+# Zad 6
+
+# a = int(input())
+# p = int(input())
+# if (a**p - a) % p == 0:
+#     print("TAK, spełnia MTF")
+# else:
+#     print("NIE, nie spełnia MTF")
+
+# Zad 7
+
+# p, k, s = map(int, input().split())
+# p = int(input())
+# k = int(input())
+# s = int(input())
+
+# if p + 3*s >= k:
+#     print("TAK, płaz przeskoczy")
+# else:
+#     print("NIE, żaba nie da rady")
