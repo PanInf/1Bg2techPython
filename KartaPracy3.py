36201c268828120f94e9589f8afba05c3edf8b7b
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-12 11:00:53 +0000

    PRE dla KartyPracy3

---
+++ KartaPracy3.py
@@ -0,0 +1,14 @@
+# Pre
+
+# pętla liczb dwucyfrowych od 10 do 21
+
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+
+# pętla liczb 3-cyfrowych podzielnych przez 20
+
+
+# Zad 1
