d8316759f02646b3443320f1763cdae3f64d9153
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-12 11:22:35 +0000

    Dodano KartaPracy3 (zad 1-2)

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -1,14 +1,24 @@
 # Pre
 
 # pętla liczb dwucyfrowych od 10 do 21
-
+# for i in range(10, 22): print(i, end=" ")
 # pętla liczb dwucyfrowych nieparzystych od 15 do 31  
-
+# for i in range(15,33,2): print(i, end=" ")
 # pętla liczb dwucyfrowych malejąco (step ujemny)
-
+# for i in range(99,9,-1): print(i, end=" ")
 # pętla liczb dwucyfrowych malejąco (step dodatni)
-
+# for i in range(10,100,1): print(109-i, end=" ")
 # pętla liczb 3-cyfrowych podzielnych przez 20
-
+# for i in range(100, 1000, 20): print(i, end=" ")
+# print()
+# for i in range(5, 50) : print(20*i, end=" ")
 
 # Zad 1
+# n = int(input())
+# for i in range(n):
+#     print(i**3 + 3, end=" ")
+
+# Zad 2
+# for i in range(105, 1000, 15) :
+#     print(i, end=" ")
+    
