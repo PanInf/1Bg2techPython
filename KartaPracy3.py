5f7c5bbe09531dc961edc4aa3e362ad9eaf1bf09
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-26 10:58:36 +0000

    Zmiana Karty Pracy 3

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -85,12 +85,12 @@
 
 # Zad 6
 
-n = int(input())
-a, b = 0, 1
+# n = int(input())
+# a, b = 0, 1
 
-for i in range(n):
-    a, b = b, a + b
-    print(a, end=" ")
+# for i in range(n):
+#     a, b = b, a + b
+#     print(a, end=" ")
 
 
 
