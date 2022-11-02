d632ec2fcf0496fd376451d9b0f2260a909245a2
PanInf <77333045+PanInf@users.noreply.github.com>
2022-11-02 13:03:00 +0000

    Wykonanie zadań PrzedSpr1.py

--- PrzedSpr1.py
+++ PrzedSpr1.py
@@ -1,14 +1,34 @@
 # Zad 1 - Medium - Oblicz sumę k początkowych liczb  trzycyfrowych
 # 100 + 101 + 102 + 103 ...
 
+# k = int(input())
+# suma = 0
+# for i in range(100, 100 + k):
+#     suma = suma + i
+# print(suma)
+
 # Zad 2 - Hard - Oblicz sumę n początkowych wyrazów ciągu fibonacciego
 # 1 + 2 + 3 + 5 + 8 + 13 ....
+
+# n = int(input())
+# a, b = 0, 1
+# suma = 0
+# for i in range(n):
+#     a, b = b, a+b
+#     suma = suma + b
+# print(suma)
 
 # Zad 3 - Hard - Spradź czy liczba wpisana przez usera jest doskonała
 # doskonała to taka, która jest równa sumie swoich dzielników bez niej samej
 # 6 = 1 + 2 + 3 (JEST)
 # 10 = 1 + 2 + 5 (NIE JEST)
 # 28 = 1 + 2 + 4 + 7 + 14 (JEST)
+# 496, 8128 - też są doskonałe
+
+
+
+
+
 
 # Zad 4 - Medium - Oblicz sumę liczb dwucyfrowych parzystych podzielnych przez 7
 # Sprawa oczywista :)
