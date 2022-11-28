e123b786f3804ea7c25436ec39c765bf9c61a0dc
PanInf <77333045+PanInf@users.noreply.github.com>
2022-11-28 12:21:28 +0000

    wykonanie alg NWD Euklides (odej i modulo)

---
+++ Algorytmy1/euklidesModulo.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+
+while b > 0:
+    a, b = b, a % b
+print(a)
---
+++ Algorytmy1/euklidesOdej.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+while a != b:
+    if a > b : a = a - b
+    if b > a : b = b - a
+print(a)
