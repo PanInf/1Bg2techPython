5d3e11566642ad248a0f42f8fef7859a793ec338
PanInf <77333045+PanInf@users.noreply.github.com>
2022-11-28 11:48:42 +0000

    Przygotowanie do pisania alg liczb pierwszych

--- Algorytmy1/pierwsze.py
+++ Algorytmy1/pierwsze.py
@@ -1,9 +1,15 @@
-# sprawdz czy dana liczba jest pierwsza
+# 1. Sprawdzenie czy liczba jest pierwsza
+# liczba pierwsza - liczba, która się dzieli tylko przez 1 i samą siebie
+# 2,3,5,7,11,13,17,19,23 .. itd
+# dzielniki właściwe - dzielniki liczby poza 1 i nią samą
 
 n = int(input())
-for i in range(2, int(n**0.5)+1):
+for i in range(2, n):
     if n % i == 0:
-        print("NIE JEST PIERWSZA")
-        exit()
+        print("Liczba nie jest pierwsza")
+        exit(0)
+print("Liczba jest pierwsza")
 
-print("TAK, LICZBA JEST PIERWSZA")
+# 2. Generowanie liczb pierwszych (w przedziale [p,q])
+
+# 3. Generowanie liczb pierwszych (pierwsze n liczb pierwszych)
