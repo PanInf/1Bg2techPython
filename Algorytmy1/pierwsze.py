3dc0b7614d04a470c71894e804457053e314b056
PanInf <77333045+PanInf@users.noreply.github.com>
2022-11-28 12:04:43 +0000

    stworzenie alg liczb pierwszych

--- Algorytmy1/pierwsze.py
+++ Algorytmy1/pierwsze.py
@@ -3,13 +3,41 @@
 # 2,3,5,7,11,13,17,19,23 .. itd
 # dzielniki właściwe - dzielniki liczby poza 1 i nią samą
 
-n = int(input())
-for i in range(2, n):
-    if n % i == 0:
-        print("Liczba nie jest pierwsza")
-        exit(0)
-print("Liczba jest pierwsza")
+# n = int(input())
+# for i in range(2, n):
+#     if n % i == 0:
+#         print("Liczba nie jest pierwsza")
+#         exit(0)
+# print("Liczba jest pierwsza")
 
 # 2. Generowanie liczb pierwszych (w przedziale [p,q])
 
+# p, q = int(input()), int(input())
+
+# for j in range(p, q+1):
+#     flaga = True
+#     for i in range(2, j):
+#         if j % i == 0:
+#             flaga = False
+#             break
+#     if flaga == True:
+#         print(j, end=" ")
+
 # 3. Generowanie liczb pierwszych (pierwsze n liczb pierwszych)
+
+n = int(input("Podaj ile chcesz liczb pierwszych: "))
+
+k = 2
+ilosc = 0
+while 1:
+    flaga = True
+    for i in range(2, k):
+        if k % i == 0:
+            flaga = False
+            break
+    if flaga == True:
+        print(k, end=" ")
+        ilosc = ilosc + 1
+    if ilosc == n:
+        break
+    k = k + 1
