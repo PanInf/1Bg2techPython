a4311364f388e802e35cbdc5c874474025ceac63
PanInf <77333045+PanInf@users.noreply.github.com>
2022-12-12 13:43:04 +0100

    Rozwiązanie części KartaPracy6

---
+++ KartyPracy/KartaPracy6.py
@@ -0,0 +1,22 @@
+# Zad 3
+
+# for i in range(99,9,-1):
+#     if i % 7 == 0:
+#         podzielnik = i
+#         break
+    
+# ilosc = 0
+# for i in range(1000, 10000):
+#     if i % podzielnik == 0:
+#         ilosc += 1
+# print(ilosc)
+
+# Zad 4
+
+# ilosc = 0
+# for i in range(10,100):
+#     cd = i // 10
+#     cj = i % 10
+#     if cd >= 2*cj:
+#         ilosc = ilosc + 1
+# print(ilosc)
