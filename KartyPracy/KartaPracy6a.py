56625f8afb9584989acf1f38691bbfad913e9d3f
PanInf <77333045+PanInf@users.noreply.github.com>
2023-01-02 12:47:39 +0000

    Dodanie zadań do KP6a (4,5)

--- KartyPracy/KartaPracy6a.py
+++ KartyPracy/KartaPracy6a.py
@@ -40,3 +40,39 @@
 #     print("TAK")
 # else:
 #     print("NIE")
+
+# Zad 3
+
+# n = int(input())
+# suma = 0
+# for i in range(1,n):
+#     if n % i == 0:
+#         suma = suma + i
+# if suma == n:
+#     print("TAK, jest doskonała")
+# else:
+#     print("NIE, nie jest doskonała")
+
+# Zad 4
+# a = int(input())
+# b = int(input())
+# x, y = a, b
+# while y > 0:
+#     x, y = y, x%y
+
+# if x == 1:
+#     print(f"TAK, {a} i {b} są względnie pierwsze")
+# else:
+#     print(f"NIE, {a} i {b} nie są względnie pierwsze")
+
+# Zad 5
+m = int(input())
+
+for i in range(10,20):
+    x, y = i, m
+    while y > 0:
+        x, y = y, x%y
+    if x == 1:
+        print(f"TAK, {i} i {m} są względnie pierwsze")
+    else:
+        print(f"NIE, {i} i {m} nie są względnie pierwsze")
